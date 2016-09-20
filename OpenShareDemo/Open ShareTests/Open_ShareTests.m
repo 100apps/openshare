a2c52435664b63deaded05009c39773ca09ff21a
dourgulf <dourgulf@gmail.com>
2016-09-20 10:11:06 +0800

    Add test case in project setting

---
+++ OpenShareDemo/Open ShareTests/Open_ShareTests.m
@@ -0,0 +1,39 @@
+//
+//  Open_ShareTests.m
+//  Open ShareTests
+//
+//  Created by jinchu darwin on 16/9/20.
+//  Copyright © 2016年 OpenShare http://openshare.gfzj.us/. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Open_ShareTests : XCTestCase
+
+@end
+
+@implementation Open_ShareTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
